c725dc0f8d614d9b5d7cf7a086ff6ddb4d0472b0
NickySimps <91176092+NickySimps@users.noreply.github.com>
2022-02-01 12:59:55 -0500

    Boobs button added

--- script.js
+++ script.js
@@ -3,15 +3,20 @@
 window.onload = init;
 
 function init() {
-    
+
 }
 function addRecipeCat() {
     let recipeCat = document.getElementById('recipeCategory');
-    const recipeCatBlock  = document.createElement('ul');
-    const recipeIngredient = document.createElement('li');
+    const recipeCatBlock = '<ul class="ingCatBlock"><li>boobs</li></ul>';
+    recipeCat.innerHTML = recipeCatBlock;
+
+    /*const recipeIngredient = document.createElement('li');
     const ingredient = document.createTextNode('Butternut Squatch');
     recipeCatBlock.appendChild(recipeIngredient);
-    recipeIngredient.appendChild(ingredient);
-    recipeCat.innerHTML = recipeCatBlock;
+    recipeIngredient.appendChild(ingredient);*/
+
+}
+
+function changeColor() {
 
 }
