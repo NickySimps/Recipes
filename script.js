5cee0f2cfbfcd811c3a91f6f52ec950e8e3ddb8c
NickySimps <91176092+NickySimps@users.noreply.github.com>
2022-01-31 21:57:26 -0500

    Recipe Cat Javascript

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 
 window.onload = init;
 
-function init {
-    let 
+function init() {
+    
+}
+function addRecipeCat() {
+    let recipeCat = document.getElementById('recipeCategory');
+    const recipeCatBlock  = document.createElement('ul');
+    const recipeIngredient = document.createElement('li');
+    const ingredient = document.createTextNode('Butternut Squatch');
+    recipeCatBlock.appendChild(recipeIngredient);
+    recipeIngredient.appendChild(ingredient);
+    recipeCat.innerHTML = recipeCatBlock;
+
 }
