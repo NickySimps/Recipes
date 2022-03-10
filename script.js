3d8f95d6833e9ea0451564067ef1984d1936d028
NickySimps <91176092+NickySimps@users.noreply.github.com>
2022-03-10 00:58:43 -0500

    new recipe cat function

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 
 
 window.onload = init;
+var ingCatCounter = 0;
 
 function init() {
 
 }
 function addRecipeCat() {
     let recipeCat = document.getElementById('recipeCategory');
-    const recipeCatBlock = '<ul class="ingCatBlock"><li>boobs</li></ul>';
-    recipeCat.innerHTML = recipeCatBlock;
+    let ingCats = document.getElementById('ingCatsSelect');
+    let ingCatSelect = ingCats.options[ingCats.selectedIndex].value;
+    let recipeCatBlock = `<ul class="ingCatBlock` + ingCatCounter + `"><li><label for="` + ingCatSelect + `">` + ingCatSelect + `</label>
+    <input type="text" name="`+ ingCatSelect + `" id="` + ingCatSelect + `" form="ingCatText" required
+        placeholder="enter the ingredients..."></input></li></ul>`;
+    if (document.getElementById(ingCatSelect) == null) {
+        recipeCat.innerHTML += recipeCatBlock;
+        ingCatCounter++;
+    }
 
-    /*const recipeIngredient = document.createElement('li');
-    const ingredient = document.createTextNode('Butternut Squatch');
-    recipeCatBlock.appendChild(recipeIngredient);
-    recipeIngredient.appendChild(ingredient);*/
+
+
+
 
 }
-
+/*
+function addIngredient {
+    let ingredient = document.getElementById(ingCatSelect).value;
+    let ingCatBlock = document.getElementsByClassName(ingCatBlock);
+    ingredient.addEventListener(onsubmit) = ingCatBlock.innerHTML += '<li>'+ingredient+'</li>';
+}
+*/
 function changeColor() {
 
 }
